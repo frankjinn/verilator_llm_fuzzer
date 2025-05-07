ad75e3d5c83d7776f4a094c4af55e3bbe66cb3b6
frank.jin <frankjin.work@gmail.com>
2025-05-07 23:39:08 +0000

    more wrapper changes

--- nodist/fuzzer/wrapper.cpp
+++ nodist/fuzzer/wrapper.cpp
@@ -23,5 +23,5 @@
 int main(int argc, char** argv, char** envp) {
     auto r = system("rm -rf obj_dir");
     assert(r == 0);
-    return execve("$VERILATOR_ROOT/bin/verilator", argv, envp);
+    return execve("/verilator/bin/verilator", argv, envp);
 }
